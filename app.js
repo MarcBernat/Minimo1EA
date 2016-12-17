81565d4cf62bbe462eb5162af3f930aa3920fb5b
Marc <marc.bernat@estudiant.upc.edu>
2016-12-18 00:43:34 +0100

    CORS Header

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//CORS header
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin');
+    res.header("Access-Control-Max-Age", "86400"); // 24 hours
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
 //Routes where api calls
 app.use('/api', api);
 
